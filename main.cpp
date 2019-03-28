ae6a8d1d9fbb98794f604309ca6405f4f95dc992
Anthony Panettiere <azezpz1@gmail.com>
2019-03-28 05:57:52 -0500

    Removing std namespace since we're already using it

--- main.cpp
+++ main.cpp
@@ -5,12 +5,12 @@ int main(int argc, char *argv[])
 {
     if (argc != 2)
     {
-        std::cerr << "The board state must be specified." << std::endl;
+        cerr << "The board state must be specified." << endl;
         return 1;
     }
 
     string state = argv[1];
-    cout << state << std::endl;
+    cout << state << endl;
 
     return 0;
 }
