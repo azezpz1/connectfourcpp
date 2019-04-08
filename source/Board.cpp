93bd2bf03e3b1ca0e659503be66b971e2deadba7
Anthony Panettiere <azezpz1@gmail.com>
2019-04-07 21:08:29 -0500

    Fixing warning: in-class initialization of non-static data member is a C++11 extension This is because we aren't allowed to initialize variables in a class definition

--- source/Board.cpp
+++ source/Board.cpp
@@ -5,48 +5,51 @@ using namespace std;
 
 class InvalidBoardState : public exception
 {
-  public:
-    InvalidBoardState(int expected_x_length, int expected_y_length, long state_length)
-    {
-        expected_x_length = expected_x_length;
-        expected_y_length = expected_y_length;
-        state_length = state_length;
-    };
+public:
+  InvalidBoardState(int expected_x_length, int expected_y_length, long state_length)
+  {
+    expected_x_length = expected_x_length;
+    expected_y_length = expected_y_length;
+    state_length = state_length;
+  };
 
-  private:
-    int expected_x_length;
-    int expected_y_length;
-    long state_length;
+private:
+  int expected_x_length;
+  int expected_y_length;
+  long state_length;
 
-    virtual const char *what() const throw()
-    {
-        string expected_length = to_string(expected_x_length * expected_y_length);
-        string valid_length = to_string(state_length);
-        string return_str = "An Invalid Board State. Given board state was ";
-        return_str += expected_length;
-        return_str += " long. But the allowed board state is ";
-        return_str += valid_length;
-        return return_str.c_str();
-    }
+  virtual const char *what() const throw()
+  {
+    string expected_length = to_string(expected_x_length * expected_y_length);
+    string valid_length = to_string(state_length);
+    string return_str = "An Invalid Board State. Given board state was ";
+    return_str += expected_length;
+    return_str += " long. But the allowed board state is ";
+    return_str += valid_length;
+    return return_str.c_str();
+  }
 };
 
 class Board
 {
-  private:
-    int x_length = 6;
-    int y_length = 7;
+public:
+  Board(string state_str)
+  {
+    allowed_x_length = 6;
+    allowed_y_length = 7;
+  }
 
-    Board(string state_str)
-    {
-    }
+private:
+  int allowed_x_length;
+  int allowed_y_length;
 
-    bool is_valid_state_length(string state)
+  bool is_valid_state_length(string state)
+  {
+    long state_length = state.length();
+    if (state_length != allowed_x_length * allowed_y_length)
     {
-        long state_length = state.length();
-        if (state_length != x_length * y_length)
-        {
-            // Invalid board state
-            throw InvalidBoardState(x_length, y_length, state_length);
-        }
+      // Invalid board state
+      throw InvalidBoardState(allowed_x_length, allowed_y_length, state_length);
     }
+  }
 };
